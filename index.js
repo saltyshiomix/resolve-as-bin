5469592537f3d9f76b9260d934e735a681e522f0
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2018-07-27 23:21:02 +0900

    Refactoring: implify

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict'
 
 var join = require('path').join
-var spawn = require('cross-spawn')
-var binPath = spawn.sync('npm', ['bin'], { cwd: process.cwd() }).stdout.toString().trim()
+var spawnSync = require('child_process').spawnSync
+var binPath = spawnSync('npm', ['bin'], { cwd: process.cwd() }).stdout.toString().trim()
 var isWin = /^win/.test(process.platform)
 
 function bin(name) {
