72dc999b63668d9b4de65d0642548d47570bd9a8
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2018-07-27 23:28:00 +0900

    Support both relative and absolute paths

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 'use strict'
 
-var join = require('path').join
+var path = require('path')
 var spawnSync = require('child_process').spawnSync
 var binPath = spawnSync('npm', ['bin'], { cwd: process.cwd() }).stdout.toString().trim()
 var isWin = /^win/.test(process.platform)
 
 function bin(name) {
-  return join(binPath, isWin ? `${name}.cmd` : name)
+  if (0 <= name.indexOf(path.sep)) {
+    return path.isAbsolute(name) ? name : path.resolve(name)
+  }
+  return path.join(binPath, isWin ? `${name}.cmd` : name)
 }
 
 module.exports = bin
