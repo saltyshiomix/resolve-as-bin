63ffa994082e76141493743bce2f8cdfd150525c
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2018-07-27 23:30:29 +0900

    Improve performance

--- index.js
+++ index.js
@@ -2,14 +2,12 @@
 
 var path = require('path')
 var spawnSync = require('child_process').spawnSync
-var binPath = spawnSync('npm', ['bin'], { cwd: process.cwd() }).stdout.toString().trim()
-var isWin = /^win/.test(process.platform)
 
 function bin(name) {
   if (0 <= name.indexOf(path.sep)) {
     return path.isAbsolute(name) ? name : path.resolve(name)
   }
-  return path.join(binPath, isWin ? `${name}.cmd` : name)
+  return path.join(spawnSync('npm', ['bin'], { cwd: process.cwd() }).stdout.toString().trim(), /^win/.test(process.platform) ? `${name}.cmd` : name)
 }
 
 module.exports = bin
