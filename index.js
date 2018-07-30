cdfbcf0923f2cc61b8cf3c11e1397800f09c980a
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2018-07-30 21:50:27 +0900

    Fix a bug on Windows

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict'
 
 var path = require('path')
-var spawnSync = require('child_process').spawnSync
+var spawn = require('cross-spawn')
 
 function bin(name) {
   if (0 <= name.indexOf(path.sep)) {
     return path.isAbsolute(name) ? name : path.resolve(name)
   }
-  return path.join(spawnSync('npm', ['bin'], { cwd: process.cwd() }).stdout.toString().trim(), /^win/.test(process.platform) ? `${name}.cmd` : name)
+  return path.join(spawn.sync('npm', ['bin'], { cwd: process.cwd() }).stdout.toString().trim(), /^win/.test(process.platform) ? `${name}.cmd` : name)
 }
 
 module.exports = bin
